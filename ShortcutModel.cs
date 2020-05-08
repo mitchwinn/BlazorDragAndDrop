7dfa46f49c3c8b5844659ec50a24b118bea186a1
Mitch Winn <mwinn22@gmail.com>
2020-05-07 22:41:52 -0700

    Initial

---
+++ ShortcutModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class ShortcutModel
+{
+    public string Grouping { get; set; }
+    public int Id { get; set; }
+    public DateTime LastUpdated { get; set; }
+    public string Link { get; set; }
+    public string LinkDescription { get; set; }
+    public string Title { get; set; }
+}
